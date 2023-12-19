40bb1be14225dd4f149db93720a86cc5963d5231
Salmoon8 <93344447+Salmoon8@users.noreply.github.com>
2023-12-19 17:37:30 +0200

    allowed hosts

--- billing/settings.py
+++ billing/settings.py
@@ -33,8 +33,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+os.environ.get("ALLOWED_HOSTS").split(" ")
 
 # Application definition
 
