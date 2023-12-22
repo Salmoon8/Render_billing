f490f126c07ecbc34c8c85dafbba99e1dadd4b74
Salmoon8 <93344447+Salmoon8@users.noreply.github.com>
2023-12-22 17:34:05 +0200

    db modify ?

--- billing/settings.py
+++ billing/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS=os.environ.get("ALLOWED_HOSTS").split(" ")
+#ALLOWED_HOSTS=os.environ.get("ALLOWED_HOSTS").split(" ")
 
 # Application definition
 
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'billing_app',
      'django.contrib.postgres',
+     'django_extensions',
 ]
 
 MIDDLEWARE = [
