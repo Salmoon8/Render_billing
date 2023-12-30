65f7e4ece7db933fc66bb92c59c517bcb68774eb
Salmoon8 <93344447+Salmoon8@users.noreply.github.com>
2023-12-30 23:13:43 +0200

    services

--- billing_app/views.py
+++ billing_app/views.py
@@ -16,11 +16,14 @@ env=environ.Env()
 def get_services_data(services_ids):
      ### replace with logic for admin api call
      # only use 5 for testing because there are no other services currently
+     print("entered services")
+     print(services_ids)
      Admin_services_url=f'https://admin-service-healu.onrender.com/api/v1/clinicService/'
      services_names=[]
      services_amounts=[]
      for id in services_ids:
           response=requests.get(f'{Admin_services_url}{id}')
+          print("services", response)
           if response.status_code== 200:
             service_data=json.loads(response.text)
             service_name=service_data['data']['clinicService']["name"]
