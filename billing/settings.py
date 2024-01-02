15683782d6c9a504a4f45962fa6561ea0ccdcabf
Salmoon8 <93344447+Salmoon8@users.noreply.github.com>
2024-01-02 15:19:37 +0200

    debug

--- billing/settings.py
+++ billing/settings.py
@@ -28,7 +28,7 @@ environ.Env.read_env(os.path.join(ENV_BASE_DIR, '.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 #ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 ALLOWED_HOSTS=os.environ.get("ALLOWED_HOSTS").split(" ")
